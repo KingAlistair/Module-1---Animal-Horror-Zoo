766daf00d82e5338c0b45442dbd49363bd45ea83
KingAlistair <60754393+KingAlistair@users.noreply.github.com>
2021-09-15 09:32:00 +0200

    Add files via upload

---
+++ LittleAnimalHorrorZooMamboNo5/src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Make rabbit and snake object from Animal class
+        Animal rabbit = new Animal("Rabbit", 3, 3);
+        Animal snake = new Animal("Snake", 9, 9);
+
+
+        //Welcome message
+        System.out.println("Welcome to the Animal Horror Zoo Demo!\n");
+
+        //Prints animals starting points
+        rabbit.print();
+        snake.print();
+
+        boolean nextTo = false;
+
+        do {
+            //Rabbit moves and prints it
+            rabbit.moveDownAndRight();
+
+            //Checks if rabbit and snakes are next to each other, if yes calls printEnding method, sets boolean nextTo to true
+            if (snake.getX() - rabbit.getX() == 1) {
+                System.out.println("I'm the rabbit please don't eat me!");
+                rabbit.printEnding(snake);
+                nextTo = true;
+
+            } else {
+                //Snake moves and prints it
+                snake.moveUpAndLeft();
+            }
+        }
+        while (!nextTo);
+    }
+}
